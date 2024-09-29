45dbfaf07c30a67971d985f2fbb87f7851600749
smmuja <smmuja27@gmail.com>
2024-09-29 23:13:20 +0700

    api hooks: usefetch base api hooks

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,32 @@
+import { baseApi } from "../config/baseApi";
+
+export async function useFetch(path, options = {}) {
+  const token = localStorage.getItem("token");
+
+  const headers = {
+    "Content-Type": "application/json",
+    ...options.headers,
+    Authorization: `Bearer ${token}`,
+  };
+
+  try {
+    const response = await fetch(`${baseApi}${path}`, {
+      ...options,
+      headers,
+    });
+
+    if (response.status === 401) {
+      localStorage.removeItem("token");
+      window.location.href = "/login";
+      alert("Login expired");
+    } else if (response.status === 403) {
+      alert("You don not have access to perform this operation");
+      window.location.href = "/";
+      return;
+    } else if (response.ok) {
+      return response.json();
+    }
+  } catch (error) {
+    throw new Error("Error occurred", error);
+  }
+}
