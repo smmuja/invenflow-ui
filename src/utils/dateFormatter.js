4c6425fd0388bba0da88c6c99a55fd430e4d34a6
smmuja <smmuja27@gmail.com>
2024-10-01 17:06:45 +0700

    add utils: date formatter

---
+++ src/utils/dateFormatter.js
@@ -0,0 +1,10 @@
+export function dateFormatter(date) {
+  const dateFormat = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  return new Date(date).toLocaleDateString("en-US", dateFormat);
+}
